f8f0d217094d5a6bec5054dee69346d9511cdfa7
Oleh Vehera <oleh.vehera@lemberg.co.uk>
2017-07-13 13:27:50 +0300

    Update MenuItemExtrasRenderTest

--- tests/src/Functional/MenuItemExtrasRenderTest.php
+++ tests/src/Functional/MenuItemExtrasRenderTest.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\Tests\menu_item_extras\Functional;
 
+use Drupal\Component\Utility\NestedArray;
 use Drupal\menu_link_content\Entity\MenuLinkContent;
+use Drupal\system\Entity\Menu;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -12,31 +14,89 @@ use Drupal\Tests\BrowserTestBase;
  */
 class MenuItemExtrasRenderTest extends BrowserTestBase {
 
-  protected $profile = 'standard';
+  /**
+   * The block under test.
+   *
+   * @var \Drupal\system\Plugin\Block\SystemMenuBlock
+   */
+  protected $block;
+
+  /**
+   * The menu for testing.
+   *
+   * @var \Drupal\system\MenuInterface
+   */
+  protected $menu;
+
+  /**
+   * Menu links info array.
+   *
+   * @var array
+   */
+  protected $links = [];
+
+  /**
+   * Amount of menu links that will generated.
+   *
+   * @var int
+   */
+  protected $linksNumber = 3;
 
   public static $modules = ['menu_item_extras'];
 
   /**
-   * Test menu item render.
+   * {@inheritdoc}
    */
-  public function testSingleItemRender() {
-    $assert = $this->assertSession();
-    $menuLink = MenuLinkContent::create([
+  protected function setUp() {
+    parent::setUp();
+    // Add a new custom menu.
+    $menu_name = 'test_menu';
+    $label = $this->randomMachineName(16);
+    $this->menu = Menu::create([
+      'id'          => $menu_name,
+      'label'       => $label,
+      'description' => $this->randomString(32),
+    ]);
+    $this->menu->save();
+    // Add block.
+    $this->block = $this->drupalPlaceBlock(
+      'system_menu_block:' . $this->menu->id(),
+      [
+        'region' => 'header',
+        'level'  => 1,
+        'depth'  => $this->linksNumber,
+      ]
+    );
+    // Set default configs for menu items.
+    $defaults = [
       'title'       => 'Extras Link',
       'link'        => 'https://example.com',
       'enabled'     => TRUE,
       'description' => 'Test Description',
-      'expanded'    => FALSE,
-      'menu_parent' => 'main:',
+      'expanded'    => TRUE,
+      'menu_name'   => $this->menu->id(),
+      'parent'      => "{$this->menu->id()}:",
       'weight'      => -10,
-      'body'        => '___ Menu Item Extras Field Value ___',
-    ]);
-    $menuLink->save();
-    $this->drupalGet('<front>');
-    $assert->statusCodeEquals(200);
-    $assert->pageTextContains('Extras Link');
-    $assert->pageTextContains('___ Menu Item Extras Field Value ___');
-    $menuLink->delete();
+      'body'        => '___ Menu Item Extras Field Value Level ___',
+    ];
+    // Generate menu items.
+    for ($i = 1; $i <= $this->linksNumber; $i++) {
+      if ($i > 1) {
+        /** @var \Drupal\menu_link_content\Entity\MenuLinkContent $previous_link */
+        $previous_link = $this->links[$i - 1]['entity'];
+      }
+      $link = MenuLinkContent::create(NestedArray::mergeDeep($defaults, [
+        'title' => $defaults['title'] . "[{$i}]",
+        'body' => $defaults['body'] . "[{$i}]",
+        'parent' => isset($previous_link) ? $previous_link->getPluginId() : $defaults['parent'],
+      ]));
+      $link->save();
+      $this->links[$i] = [
+        'title'  => $link->get('title')->getString(),
+        'body'   => $link->get('body')->getString(),
+        'entity' => $link,
+      ];
+    }
   }
 
   /**
@@ -44,56 +104,11 @@ class MenuItemExtrasRenderTest extends BrowserTestBase {
    */
   public function testMultilevelItems() {
     $assert = $this->assertSession();
-    $menu = \Drupal::entityTypeManager()
-      ->getStorage('menu')
-      ->load('main');
-    $this->assertNotEmpty($menu, 'Main menu exists');
-    $link1 = MenuLinkContent::create([
-      'title'       => 'Extras Link 1',
-      'link'        => 'https://example.com',
-      'enabled'     => TRUE,
-      'description' => 'Test Description',
-      'expanded'    => TRUE,
-      'menu_parent' => "{$menu->id()}:",
-      'weight'      => -10,
-      'body'        => '___ Menu Item Extras Field Value Level 1 ___',
-    ]);
-    $link1->save();
-    $link2 = MenuLinkContent::create([
-      'title'       => 'Extras Link 2',
-      'link'        => 'https://example.com',
-      'enabled'     => TRUE,
-      'description' => 'Test Description',
-      'expanded'    => TRUE,
-      'menu_parent' => "{$menu->id()}:{$link1->getPluginId()}:{$link1->uuid()}",
-      'weight'      => -10,
-      'body'        => '___ Menu Item Extras Field Value Level 2 ___',
-    ]);
-    $link2->save();
-    $link3 = MenuLinkContent::create([
-      'title'       => 'Extras Link 3',
-      'link'        => 'https://example.com',
-      'enabled'     => TRUE,
-      'description' => 'Test Description',
-      'expanded'    => TRUE,
-      'menu_parent' => "{$menu->id()}:{$link2->getPluginId()}:{$link2->uuid()}",
-      'weight'      => -10,
-      'body'        => '___ Menu Item Extras Field Value Level 3 ___',
-    ]);
-    $link3->save();
     $this->drupalGet('<front>');
-    $assert->statusCodeEquals(200);
-
-    $assert->pageTextContains('Extras Link 1');
-    $assert->pageTextContains('Extras Link 2');
-    $assert->pageTextContains('Extras Link 3');
-    $assert->pageTextContains('___ Menu Item Extras Field Value Level 1 ___');
-    $assert->pageTextContains('___ Menu Item Extras Field Value Level 2 ___');
-    $assert->pageTextContains('___ Menu Item Extras Field Value Level 3 ___');
-
-    $link1->delete();
-    $link2->delete();
-    $link3->delete();
+    foreach ($this->links as $link) {
+      $assert->pageTextContains($link['title']);
+      $assert->pageTextContains($link['body']);
+    }
   }
 
 }

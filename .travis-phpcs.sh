cafc9b938552349daed1b9b995c2871426cc3d87
Bogdan Hepting[alias] <bogdan.hepting@lemberg.co.uk>
2017-07-18 14:42:01 +0300

    Updated configs for code sniffer

--- .travis-phpcs.sh
+++ .travis-phpcs.sh
@@ -6,4 +6,4 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-phpcs --report=full --warning-severity=0 --runtime-set ignore_warnings_on_exit false --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || true
+phpcs --report=full --warning-severity=0 --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false

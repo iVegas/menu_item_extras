e6c86d1d1f2515972e4cebea9814c5e16518dd05
Andriy Khomych <andriy.khomych@gmail.com>
2017-08-10 17:36:33 +0300

    Enabled warning for DrupalPractice in travis.

--- .travis-phpcs.sh
+++ .travis-phpcs.sh
@@ -9,4 +9,4 @@ drupal_ti_ensure_drupal
 echo "codesniffer standard Drupal"
 phpcs --report=full --warning-severity=0 --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
 echo "codesniffer standard DrupalPractice"
-phpcs --report=full --warning-severity=0 --standard=DrupalPractice "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
+phpcs --report=full --standard=DrupalPractice "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false

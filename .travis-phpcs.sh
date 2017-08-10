7f7c53da62033528fe4fcd7518f85fddc97164b6
Andriy Khomych <andriy.khomych@gmail.com>
2017-08-10 17:27:26 +0300

    Added codesniffer updates for travis.

--- .travis-phpcs.sh
+++ .travis-phpcs.sh
@@ -7,6 +7,6 @@ set -e $DRUPAL_TI_DEBUG
 drupal_ti_ensure_drupal
 
 echo "codesniffer standard Drupal"
-#phpcs --report=full --warning-severity=0 --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
+phpcs --report=full --warning-severity=0 --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
 echo "codesniffer standard DrupalPractice"
-phpcs --report=full --warning-severity=0 --standard=~/.composer/vendor/drupal/coder/coder_sniffer/DrupalPractice "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
+phpcs --report=full --warning-severity=0 --standard=DrupalPractice "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false

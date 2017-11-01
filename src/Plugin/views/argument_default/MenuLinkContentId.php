1641c016184617475fa3094a3f5908412366c9b0
Oleh Vehera <oleh.vehera@lemberg.co.uk>
2017-11-01 18:23:06 +0200

    Update MenuLinkContentId.php

--- src/Plugin/views/argument_default/MenuLinkContentId.php
+++ src/Plugin/views/argument_default/MenuLinkContentId.php
@@ -52,7 +52,7 @@ class MenuLinkContentId extends ArgumentDefaultPluginBase implements CacheableDe
    * @param string $plugin_id
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
-   *   The plugin implementation definition.   *
+   *   The plugin implementation definition.
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The route match.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager

10f00420cd089725954aaac8a1edf6c5775dfb22
voleger <voleger@3260314.no-reply.drupal.org>
2019-01-09 12:17:00 +0200

    Issue #3024706 by voleger: Fix MenuLinksTests

--- tests/src/Kernel/MenuLinksOriginTest.php
+++ tests/src/Kernel/MenuLinksOriginTest.php
@@ -2,21 +2,23 @@
 
 namespace Drupal\Tests\menu_item_extras\Kernel;
 
-use Drupal\Tests\menu_link_content\Kernel\MenuLinksTest;
-
-/**
- * Tests handling of menu links hierarchies.
- *
- * @group menu_item_extras
- */
-class MenuLinksOriginTest extends MenuLinksTest {
+if (class_exists("\Drupal\Tests\menu_link_content\Kernel\MenuLinksTest")) {
 
   /**
-   * {@inheritdoc}
+   * Tests handling of menu links hierarchies.
+   *
+   * @group menu_item_extras
    */
-  public function __construct($name = NULL, array $data = [], $dataName = '') {
-    static::$modules[] = 'menu_item_extras';
-    parent::__construct($name, $data, $dataName);
+  class MenuLinksOriginTest extends \Drupal\Tests\menu_link_content\Kernel\MenuLinksTest {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($name = NULL, array $data = [], $dataName = '') {
+      static::$modules[] = 'menu_item_extras';
+      parent::__construct($name, $data, $dataName);
+    }
+
   }
 
 }
